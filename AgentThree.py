2f91010768a4a44e3d30d03d7fc89ed3c2ba9b9a
Abhishek Sawalkar <abhishek31sawalkar@gmail.com>
2022-11-07 20:46:17 -0500

    Agent 3 mark 1 achieved

--- AgentThree.py
+++ AgentThree.py
@@ -7,6 +7,8 @@ class AgentThree:
     
     def __init__(self,n_nodes,G : nx.Graph,prey:Prey, predator:Predator):
         self.n_nodes=n_nodes
+        self.prey=prey
+        self.predator=predator
 
         ag_not_decided=True
         while ag_not_decided:
@@ -15,63 +17,181 @@ class AgentThree:
                 ag_not_decided=False
 
         self.G=G
+        self.p_now=[0]*n_nodes
+        self.p_next=[0]*n_nodes
+        # Initialize the probablities of all the nodes in the graph
+        self.initialize_probabilities()
+        # self.p_now[5]=1
+        # self.p_now[self.position-1]=0
+        # print(*self.p_now)
 
     def simulate_step(self,prey : Prey,predator:Predator):
         #Agent is only aware of the predator's position. so we can only effectively use its value
         #Prey's position here is only used to check if the surveyed node is the prey's node or not
 
-        d_predator=len(get_bfs_path(self.G, self.position, predator.position)[1])  #Distance from predator
+        # d_predator=len(get_bfs_path(self.G, self.position, predator.position)[1])  #Distance from predator
 
         neighbor_list=list(self.G.neighbors(self.position))
+   
+        # self.initialize_probabilities()
         
-        # print("Distance From Prey = ",d_prey)
-        # print("Distance from Pred =",d_predator)
-        # print("Current Position = ",self.position)
-        # print("Neighbor List = ",neighbor_list)
+        # self.print_state()
 
-        #initialize the probablities of all the nodes in the graph
-        self.initialize_probabilities()
-
-        while(True):
+        prey_found=False
+        # while(not prey_found):
         #randomly survey a node
-            if prey.position==self.position:
-                print("Prey found")
-            survey_list=list(range(1,51))
-            survey_list.remove(self.position)
-            survey_node=random.choice(survey_list)
-
-            if survey_node==prey.position:
-                self.G.nodes[survey_node]["P_now"]=1
-                #set prob of all other nodes to 0
-
-                for node in range(1,51):
-                    if node!=survey_node:
-                        self.G.nodes[node]["P_now"]=0
-                
-                set_next_prob_list=list(self.G.neighbors(survey_node))+survey_node
-
-                for node in set_next_prob_list:
-                    # if self.G.degree(node)==3:
-                    set_next_prob_list_neightbor=list(self.G.neighbors(node))+node
-                    for node_2 in set_next_prob_list_neightbor:
-                        if self.G.degree(node_2)==3:
-                            multiplier=1/4
-                        else:
-                            multiplier=1/3
-                        P_next+=self.G.nodes[node_2]["P_now"]*multiplier
-                    
-                    self.G.nodes[node]["P_next"]=P_next
-                
-                
-                    
-
-
-
-
+        if prey.position==self.position:
+            print("Prey found")
+            prey_found=True
+            return
         
+        survey_list=list(range(1,51))
+        survey_list.remove(self.position)
+        survey_node=random.choice(survey_list)
+        # survey_node=5
+        # Belief update based on surveyed node
+        self.update_belief(survey_node, prey.position)
+        # self.print_state()
+
+        #Agent moves towards the highest prob_now node--
+        m=max(self.p_now)
+        max_prob_list=[node+1 for node in range(len(self.p_now)) if self.p_now[node]==m]
+        target_node=random.choice(max_prob_list)
+
+        path_to_target=get_bfs_path(G, self.position, target_node)[1]
+
+        self.position=path_to_target[1]
+        #Agent has now moved to the new position
+        #Has agent reached the prey and won?
+        if self.position==prey.position:
+            print("Prey found")
+            prey_found=True
+            return
+        #If not won, update belief system again
+        self.update_belief(self.position, prey.position)
+        # self.print_state()
+
+        # #Now prey will move
+        # prey.simulate_step()
+    
+    
+
+    def update_belief(self,survey_node,prey_positon):
+        
+        if survey_node==prey_positon:
+            # self.G.nodes[survey_node]["P_now"]=1
+            self.p_now[survey_node-1]=1
+            #set prob of all other nodes to 0
+
+            for node in range(1,51):
+                if node!=survey_node:
+                    self.p_now[node-1]=0
+                    # self.G.nodes[node]["P_now"]=0
+            
+            self.transition_update()
+            self.p_now=self.p_next
+            print("After locating prey -> ",*self.p_next)
+        else:
+            # self.G.nodes[survey_node]["P_now"]=0
+            print("Survey node:",survey_node)
+            # print("Current node:",survey_node)
+            p_new=[0]*50
+            p_prey_not_in_survey_node=1-self.p_now[survey_node-1]
+            self.p_next[survey_node-1]=0
+
+            for node in range(1,51):
+                if node!=survey_node:
+                    # p_prey_not_in_survey_node=1-self.G.nodes[survey_node]["P_now"]
+                    # p_prey_in_current_node=self.G.nodes[node]["P_now"]
+                    p_prey_in_current_node=self.p_now[node-1]
+                    p_new[node-1]=p_prey_in_current_node/p_prey_not_in_survey_node
+            
+            self.p_now=p_new
+            self.transition_update()
+            self.p_next=self.p_now
+            
+
+    def transition_update(self):
+        for survey_node in range(1,n_nodes+1):
+            set_next_prob_list=list(self.G.neighbors(survey_node))+[survey_node]
+
+            for node in set_next_prob_list:
+                # if self.G.degree(node)==3:
+                set_next_prob_list_neightbor=list(self.G.neighbors(node))+[node]
+                p_node_2=0
+                for node_2 in set_next_prob_list_neightbor:
+                    if self.G.degree(node_2)==3:
+                        multiplier=4
+                    else:
+                        multiplier=3
+                    # P_next_calc+=self.G.nodes[node_2]["P_now"]*multiplier
+                    p_node_2+=self.p_now[node_2-1]/multiplier
+                
+                # self.G.nodes[node]["P_next"]=P_next
+                # self.p_next[node-1]=p_node_2
+                self.p_next[node-1]=p_node_2
 
     def initialize_probabilities(self):
         for node in range(self.n_nodes):
-            self.G.nodes[node][prob_now]=1/49
+            self.p_now[node]=1/49
+            # self.G.nodes[node][prob_now]=1/49
+
+    def print_state(self):
+        print("\nCurrent State ->")
+        print("Prey Position : ",self.prey.position)
+        print("Agent Position : ",self.position)
+        d_prey=len(get_bfs_path(self.G, self.position, self.prey.position)[1])  #Distance from prey
+        print("Distance to Prey : ",d_prey)
+        print("Sum of P_now : ",sum(self.p_now))
+        # print("P_now -> ",*self.p_now)
+        # print()
+        # print("Sum of P_next : ",sum(self.p_next))
+        # print("P_next -> ",self.p_next)
+        print("\n")
     
+        
     
+if __name__=="__main__":
+    n_nodes=50
+    G=Graph(n_nodes).G
+    prey=Prey(n_nodes,G)
+    # prey.position=6
+    predator=Predator(n_nodes, G)
+    agent_three=AgentThree(n_nodes, G, prey, predator)
+    # agent_three.simulate_step(prey, predator)
+    for i in range(0,100):
+    # while(True):
+        if agent_three.position==prey.position:
+            print("Prey found")
+            break
+        agent_three.simulate_step(prey, predator)
+        agent_three.print_state()
+
+    # agent_three.print_state()
+    # for i in range(0,10):
+    #     agent_three.transition_update()
+    #     agent_three.print_state()
+
+    
+
+
+
+
+# def transition_update(self,survey_node):
+#         set_next_prob_list=list(self.G.neighbors(survey_node))+[survey_node]
+
+#         for node in set_next_prob_list:
+#             # if self.G.degree(node)==3:
+#             set_next_prob_list_neightbor=list(self.G.neighbors(node))+[node]
+#             p_node_2=0
+#             for node_2 in set_next_prob_list_neightbor:
+#                 if self.G.degree(node_2)==3:
+#                     multiplier=1/4
+#                 else:
+#                     multiplier=1/3
+#                 # P_next_calc+=self.G.nodes[node_2]["P_now"]*multiplier
+#                 p_node_2+=self.p_now[node_2-1]*multiplier
+            
+#             # self.G.nodes[node]["P_next"]=P_next
+#             # self.p_next[node-1]=p_node_2
+#             self.p_now[node-1]=p_node_2
